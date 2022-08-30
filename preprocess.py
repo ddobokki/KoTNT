064fe529c6c9762a3d4671c7c8b38695986d9c2b
ddobokki <ddobokki@naver.com>
2022-08-30 16:38:16 +0900

    preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,28 @@
+import re
+
+from utils.sent_filters import sentence_filter
 from utils.TRNDataLoader import TRNDataLoader
 
 loader = TRNDataLoader("data/raw")
-i = 0
+sent_pair = []
 for sent in loader.generate():
-    i += 1
+    brackets = re.findall("\(.*?\)", sent)  # 괄호 찾음
+    if brackets and len(brackets) % 2 == 0:  # bracket이 있고
+        for i in range(0, len(brackets), 2):
+            if not bool(re.search(r"\d", brackets[i])):
+                sent = sent.replace(brackets[i + 1], brackets[i])
+    try:
+        sent1 = sentence_filter(sent, mode="spelling")
+        if not bool(re.search(r"\d", sent1)):
+            # 숫자가 없으면
+            continue
+        sent2 = sentence_filter(sent, mode="phonetic")
+        sent_pair.append([sent1, sent2])
+    except:
+        pass
 
-print(i)
+# [TODO]
+# 1. 전사 규칙 잘 못된 애들 필터링 ex 1주일이 (1주일) / (일주일)로 안 되어 있는 문장
+# 2. 알고리즘 효율화(현재 상태 - 느림)
+
+print(len(sent_pair))
---
+++ utils/sent_filters.py
@@ -0,0 +1,73 @@
+import re
+
+
+def bracket_filter(sentence, mode="phonetic"):
+    new_sentence = str()
+
+    if mode == "phonetic":
+        flag = False
+
+        for ch in sentence:
+            if ch == "(" and flag is False:
+                flag = True
+                continue
+            if ch == "(" and flag is True:
+                flag = False
+                continue
+            if ch != ")" and flag is False:
+                new_sentence += ch
+        if flag:
+            raise ValueError("Unsupported mode : {0}".format(sentence))
+
+    elif mode == "spelling":
+        flag = True
+
+        for ch in sentence:
+            if ch == "(":
+                continue
+            if ch == ")":
+                if flag is True:
+                    flag = False
+                    continue
+                else:
+                    flag = True
+                    continue
+            if ch != ")" and flag is True:
+                new_sentence += ch
+
+    else:
+        raise ValueError("Unsupported mode : {0}".format(mode))
+
+    return new_sentence
+
+
+def special_filter(sentence, mode="phonetic", replace=None):
+    SENTENCE_MARK = ["?", "!", "."]
+    NOISE = ["o", "n", "u", "b", "l"]
+    EXCEPT = ["/", "+", "*", "-", "@", "$", "^", "&", "[", "]", "=", ";", ","] + SENTENCE_MARK
+
+    new_sentence = str()
+    for idx, ch in enumerate(sentence):
+        if ch not in SENTENCE_MARK:
+            if idx + 1 < len(sentence) and ch in NOISE and sentence[idx + 1] == "/":
+                continue
+
+        if ch == "#":
+            new_sentence += "샾"
+
+        elif ch == "%":
+            if mode == "phonetic":
+                new_sentence += replace
+            elif mode == "spelling":
+                new_sentence += "%"
+
+        elif ch not in EXCEPT:
+            new_sentence += ch
+
+    pattern = re.compile(r"\s\s+")
+    new_sentence = re.sub(pattern, " ", new_sentence.strip())
+    return new_sentence
+
+
+def sentence_filter(raw_sentence, mode, replace=None):
+    return special_filter(bracket_filter(raw_sentence, mode), mode, replace)
