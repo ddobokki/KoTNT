96380ae2d873019b07ca6d5e4d775604cd4bdf9d
YooSungHyun <shyu0522@hanmail.net>
2022-10-03 12:51:51 +0900

    Remove: datasets는 vocab 다양성 등 이슈로 사용하지 않기로함.

--- datasets_preproc.sh
+++
@@ -1,8 +0,0 @@
-# /bin/bash
-
-python3 /data2/bart/temp_workspace/nlp/KoGPT_num_converter/datasets_preprocess.py \
-    --data_dir=/data2/bart/temp_workspace/nlp/KoGPT_num_converter/data/raw \
-    --datasets_name=bart \
-    --model_name_or_path=/data2/bart/temp_workspace/nlp/models/kobart-base-v2 \
-    --output_dir=/data2/bart/temp_workspace/nlp/bart_datasets \
-    --datasets_generator_path=/data2/bart/temp_workspace/nlp/KoGPT_num_converter/utils/DatasetsGenerator.py
