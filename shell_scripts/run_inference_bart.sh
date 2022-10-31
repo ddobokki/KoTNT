ad6d9b7c1da2a265df7e67832a07946b845e2bf4
YooSungHyun <shyu0522@hanmail.net>
2022-10-31 14:25:34 +0900

    Update: 추가된 파라미터와 민감경로 제거

--- shell_scripts/run_inference_bart.sh
+++ shell_scripts/run_inference_bart.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 python3 ../bart_inference.py \
-	--model_name_or_path="/data2/bart/temp_workspace/nlp/output_dir/checkpoint-30000" \
+	--model_name_or_path="" \
 	--beam_width="100" \
 	--min_length="0" \
 	--max_length="1206" \
 	--do_sample="false" \
-	--num_beam_groups="1"
+	--num_beam_groups="1" \
+	--direction="backward"
