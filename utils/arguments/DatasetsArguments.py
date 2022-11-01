68f7843df8c27e671b45e0f9eb2ef99d511e0d89
jp1924 <jp42maru@gmail.com>
2022-11-01 10:18:34 +0900

    Add: T5 arguments

--- utils/arguments/DatasetsArguments.py
+++ utils/arguments/DatasetsArguments.py
@@ -1,12 +1,30 @@
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import List, Optional, Union
 
 
 @dataclass
 class DatasetsArguments:
-    train_csv_paths: Optional[List[str]] = field(default=None)
-    valid_csv_paths: Optional[List[str]] = field(default=None)
-    temp_datasets_dir: Optional[str] = field(default=None)
+    train_csv_paths: Union[Optional[List[str]], str] = field(
+        default=None,
+        metadata={"help": ""},
+    )
+    valid_csv_paths: Union[Optional[List[str]], str] = field(
+        default=None,
+        metadata={"help": ""},
+    )
+    max_length: int = field(
+        default=512,
+        metadata={"help": ""},
+    )
+    num_proc: int = field(
+        default=1,
+        metadata={"help": ""},
+    )
+    temp_datasets_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": ""},
+    )
     vocab_path: str = field(
         default=None,
+        metadata={"help": ""},
     )
